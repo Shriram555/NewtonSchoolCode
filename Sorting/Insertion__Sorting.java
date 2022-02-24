c3b3e238139fccda0da6cd2a697b09a2ae593a8a
Shriram555 <100347859+Shriram555@users.noreply.github.com>
2022-02-24 15:18:01 +0530

    Add files via upload

---
+++ Sorting/Insertion__Sorting.java
@@ -0,0 +1,14 @@
+public  class  java.awt.Button extends  java.awt.Component  
+	{
+// 	Constructors
+		public Button();	
+		//	Creates a button with no label. 
+		public Button(String  label);	
+		//	Creates a button with the indicated label. 
+
+		// 	Methods
+		public void addNotify();	 
+		public String getLabel();	 
+		protected String paramString();	 
+		public void setLabel(String  label);	 
+	}
---
+++ Sorting/PrimeNo.java
@@ -0,0 +1,6 @@
+import java.lang.System.*;
+public class PrimeNo {
+     public static void main(String args[]){
+   out.println("JAVA");
+     } 
+}
