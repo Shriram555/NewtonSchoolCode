1b90a5d07779b23a22102f0e537d3cb3ce7ca718
Shriram555 <100347859+Shriram555@users.noreply.github.com>
2022-02-25 21:48:10 +0530

    Add files via upload

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,16 +1,17 @@
-class MergeS{
-	boolean FindPrime(int n){
-       for (int i=2;i<n;i++){
-		   if(n%i==0) return false;
-		   else return true;
-	   }
+
+public class Array{
+	static int powRec(int a, int b){
+		if(b==0) return 2;
+		
+		if(b%2==0){
+			return powRec(a, b/2)*powRec(a, b/2);
+		}else{
+	      return a*powRec(a, b/2)*powRec(a, b/2);
+		}
 	}
+public static void main(String args[]){
+  int a=2;
+  int b=10;
+  System.out.println(powRec(a,b));
 }
-class MergeSort{
-	public static void main(String args[]){
-		MergeS sc=new MergeS();
-           boolean a=sc.FindPrime(14);
-     if(a==true)System.out.println("prime");
-	 else System.out.println("not prime");
-	}
 }
