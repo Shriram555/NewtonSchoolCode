8cd9090523876f5874fdac5d5ef92efc86859086
Shriram555 <100347859+Shriram555@users.noreply.github.com>
2022-04-16 17:26:58 +0530

    Add files via upload

---
+++ AWT/MyText.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+class MyText {
+	public static void main(String args[]){
+		Frame s=new Frame("java");
+		TextField text1;
+		text1=new TextField(" iam from pune i have complete be in mechanichal engineering");
+		text1.setBounds(60,150,200,40);
+		s.add(text1);
+		s.setSize(300,300);
+		s.setLayout(null);
+		s.setVisible(true);
+		
+		
+		  
+		
+		}
+
+}
